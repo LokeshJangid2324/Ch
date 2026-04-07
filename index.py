8a4d34754aa71d892cf823a3e6ce63917d0652a2
LokeshJangid2324 <lokeshjangid2324@gmail.com>
2026-04-07 18:12:57 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,17 @@
+name=input("ENTER YOUR NAME : ")
+print(name +" Welcome To My First Python Program "  )
+
+age=input("ENTER YOUR AGE : ")
+gen=input("ENTER YOUR GENDER : ")
+cas=input("ENTER YOUR CAST : ")
+
+no=input("ENTER YOUR MOBILE NOMBER :")
+mail=input("ENTER YOUR MAIL ADDRESS :")
+
+print("USER DETAIL\n")
+print(age )
+print(gen)
+print(cas)
+print(no)
+print(mail)
+print("Thank You For Visit My Program")
